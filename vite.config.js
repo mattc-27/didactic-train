19ddaa499818ecc932420b21893867a17b2ae251
mattc-27 <108201614+mattc-27@users.noreply.github.com>
2024-07-16 22:34:17 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: '/',
+  base: '/didactic-train/,
   build: {
     outDir: 'dist',
     rollupOptions: {
